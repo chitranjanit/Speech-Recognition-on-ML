5cdb43e7a4f6dfbd90680d5751bb6942a1d30724
Chitranjan <chitranjan@naphix.com.au>
2020-05-27 17:07:12 +0545

    Test

---
+++ readme.txt
@@ -0,0 +1,8 @@
+
+
+## First install ##
+pip3 install SpeechRecognition
+
+
+## Second Library ##
+pip3 install pyaudio
---
+++ step1.py
@@ -0,0 +1,8 @@
+import speech_recognition as sr
+filename='speech.wav'
+r=sr.Recognizer()
+
+with sr.AudioFile(filename) as source:
+	audio_data = r.record(source)
+	text=r.recognize_google(audio_data)
+	print(text)
